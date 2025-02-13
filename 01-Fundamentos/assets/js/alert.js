0113d8e03637e3efd13947c64c85bd78a649a296
JulioRiveraAguilar <161143868+JulioRiveraAguilar@users.noreply.github.com>
2025-02-12 21:41:55 -0600

    seccion 2 completa

---
+++ 01-Fundamentos/assets/js/alert.js
@@ -0,0 +1,11 @@
+//alert('Hola mundo')
+
+
+//let nombre = prompt('¿Cual es tu nombte?');
+//console.log(nombre);
+//console.log( '****'+ nombre + '****'); 
+
+//const seleccion = confirm('¿Estas seguro de borrar esto?');
+//console.log( seleccion );
+
+console.log( global);
---
+++ 01-Fundamentos/assets/js/app.js
@@ -0,0 +1,27 @@
+//alert('hola desde .js')
+
+//console.log('Hola mundo');
+
+// para crea una variable
+// var b = 10;
+// const c = 10; 
+let a = 10, 
+    b = 20, 
+    c = 'hola ', 
+    d = 'Spiderman';
+let x = a + b ;
+const saludo = c + d;
+
+//console.log('%c Mis variables', 'color:red; font-weight: bold');
+//console.log({a});
+//console.log('b',b);
+//console.log(c);
+//console.warn(x); //para colocar un aviso
+//console.error(x); //para colocar como si fuera un error 
+
+c = 'Hola de nuevo';
+
+//console.table({ a, b, c, d, x});
+
+
+let miNuevoNombre = 'Julio Rivera';
