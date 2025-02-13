950986924fe40673c78973c939e7939297037e8e
JulioRiveraAguilar <161143868+JulioRiveraAguilar@users.noreply.github.com>
2025-02-12 22:03:34 -0600

    comenzando la seccion 3

---
+++ 01-Fundamentos/assets/js/Primitivos.js
@@ -0,0 +1,9 @@
+let nombre = 'Peter parker';
+console.log(nombre);
+
+// si se le colocar let de nuveo daria error al estar declarado antes
+nombre = 'Ben parker';
+console.log(nombre);
+
+nombre= "Tia May";
+
