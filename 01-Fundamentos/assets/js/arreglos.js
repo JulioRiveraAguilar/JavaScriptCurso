2c471a662520785d1297286cbea56c973cb34f81
JulioRiveraAguilar <161143868+JulioRiveraAguilar@users.noreply.github.com>
2025-02-13 19:32:09 -0600

    Arreglos

---
+++ 01-Fundamentos/assets/js/arreglos.js
@@ -0,0 +1,22 @@
+
+//const arr= new Array(10);
+//const arr=[];
+//console.log(arr);
+
+let videojuegos = [ 'Mario 3', 'megaman', ' chrono Trigger' ];
+console.log({videojuegos});
+
+//para pedir una posicion exacta del arreglo recordar comienzan con 0
+console.log(videojuegos[0]);
+
+let arregloCosas = [
+    true,123,'Fernando', 1+2, // o el arreglo final puede estar sin la ,
+    function(){},
+    ()=>{},
+    {a:1},
+    ['x', 'Megaman','zero', 'DR.Light']
+];
+
+//console.log({arregloCosas});
+//console.log(arregloCosas[2]);
+console.log(arregloCosas[7][3]);
---
+++ 01-Fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,11 @@
+//let 1ab = 123; no deja por el numero
+
+let _objeto$ = 123;
+
+// let previo_99.99 = 123;
+
+let jugardorConpuntajeMasAlto= 'Fernando' // es mejor usar nombre signiquitativos
+
+class JuegoAnio{
+
+}
